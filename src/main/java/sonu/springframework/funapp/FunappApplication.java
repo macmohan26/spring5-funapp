70155555ca062d1f7923c911d592b7dbbb684b47
mrathore <mrathore@unomaha.edu>
2018-08-12 09:09:00 -0500

    Initial Commit.

---
+++ src/main/java/sonu/springframework/funapp/FunappApplication.java
@@ -0,0 +1,12 @@
+package sonu.springframework.funapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunappApplication.class, args);
+    }
+}
---
+++ src/test/java/sonu/springframework/funapp/FunappApplicationTests.java
@@ -0,0 +1,16 @@
+package sonu.springframework.funapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
